c2f704d5689fdb6bbb169e9415ea1bea18424c7b
Diego Lopes <diegoplopes5@gmail.com>
2021-03-28 18:01:28 -0300

    feat: add mock topics response

--- src/main/java/com/api/application/controllers/TopicController.java
+++ src/main/java/com/api/application/controllers/TopicController.java
@@ -1,9 +1,23 @@
 package com.api.application.controllers;
 
+import com.api.application.entities.responses.TopicResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/topics")
 public class TopicController {
+
+    @GetMapping()
+    public List<TopicResponse> getAllTopics() {
+        return Arrays.asList(
+                new TopicResponse("spring", "Spring Framework", "Learn Spring Faster!"),
+                new TopicResponse("adonis", "Adonis Framework", "Learn Adonis Faster!"),
+                new TopicResponse("react", "React Lib", "Learn React Faster!")
+        );
+    }
 }
