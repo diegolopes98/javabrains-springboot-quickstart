b8797a7373fb6e437725ab9a237a5b9a8b93b43b
Diego Lopes <diegoplopes5@gmail.com>
2021-04-12 04:41:10 -0300

    feat: add course entity

---
+++ src/main/java/com/api/application/domain/entity/CourseEntity.java
@@ -0,0 +1,59 @@
+package com.api.application.domain.entity;
+
+import com.api.application.domain.model.CourseModel;
+import com.api.application.domain.model.TopicModel;
+import com.api.application.domain.protocol.topic.TopicInterface;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = CourseEntity.TABLE_NAME)
+public class CourseEntity implements TopicInterface{
+    public static final String TABLE_NAME = "COURSES";
+
+    public CourseEntity() {
+    }
+
+    public CourseEntity(CourseModel courseModel) {
+        this.id = courseModel.getId();
+        this.name = courseModel.getName();
+        this.description = courseModel.getDescription();
+    }
+
+    public CourseEntity(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    @Id
+    private String id;
+    private String name;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        if(this.id == null) this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
