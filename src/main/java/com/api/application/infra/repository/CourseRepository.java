a91697db4386f903b358ebc32bab58398351f6e7
Diego Lopes <diegoplopes5@gmail.com>
2021-04-13 03:58:51 -0300

    refactor: find by topic id instead of topic in getAllCoursesUseCase

--- src/main/java/com/api/application/infra/repository/CourseRepository.java
+++ src/main/java/com/api/application/infra/repository/CourseRepository.java
@@ -1,11 +1,10 @@
 package com.api.application.infra.repository;
 
 import com.api.application.domain.entity.CourseEntity;
-import com.api.application.domain.entity.TopicEntity;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface CourseRepository extends CrudRepository<CourseEntity, String> {
-    List<CourseEntity> findByTopic(TopicEntity topic);
+    List<CourseEntity> findByTopicId(String topicId);
 }
--- src/main/java/com/api/application/usecase/course/GetAllCoursesUseCase.java
+++ src/main/java/com/api/application/usecase/course/GetAllCoursesUseCase.java
@@ -1,12 +1,9 @@
 package com.api.application.usecase.course;
 
-import com.api.application.domain.entity.TopicEntity;
 import com.api.application.domain.model.CourseModel;
 import com.api.application.domain.model.TopicModel;
 import com.api.application.domain.protocol.entity.GetAllEntitiesInterface;
 import com.api.application.infra.repository.CourseRepository;
-import com.api.application.infra.repository.TopicRepository;
-import com.api.application.presentation.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,20 +16,11 @@ public class GetAllCoursesUseCase implements GetAllEntitiesInterface<CourseModel
     @Autowired
     CourseRepository courseRepository;
 
-    @Autowired
-    TopicRepository topicRepository;
-
-    public <PID> List<CourseModel> getAll(PID parentId) throws NotFoundException {
+    public <PID> List<CourseModel> getAll(PID parentId) {
         List<CourseModel> allCourses = new ArrayList<CourseModel>();
 
-        TopicEntity parentTopic = topicRepository.findById((String) parentId).orElse(null);
-
-        if(parentTopic == null) {
-            throw new NotFoundException();
-        }
-
         courseRepository
-                .findByTopic(parentTopic)
+                .findByTopicId((String) parentId)
                 .forEach(
                         courseEntity -> allCourses
                                 .add(
