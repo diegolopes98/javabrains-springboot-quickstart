3186bb1200faeeac7b2d5f97542b8c410c4e26c1
Diego Lopes <diegoplopes5@gmail.com>
2021-05-17 13:55:33 -0300

    test: course entity getters and setter

--- src/test/java/com/api/application/domain/entity/CourseEntityTest.java
+++ src/test/java/com/api/application/domain/entity/CourseEntityTest.java
@@ -1,5 +1,8 @@
 package com.api.application.domain.entity;
 
+import com.api.application.domain.model.CourseModel;
+import com.api.application.domain.model.TopicModel;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,9 +15,28 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class CourseEntityTest {
+
+    final String TEST_ID = "test id";
+    final String TEST_NAME = "test name";
+    final String TEST_DESCRIPTION = "test description";
+
+    CourseModel courseModelMock = mock(CourseModel.class);
+    TopicModel topicModelMock = mock(TopicModel.class);
+    TopicEntity topicEntityMock = mock(TopicEntity.class);
+
+    @BeforeEach
+    void setUpMocks() {
+        when(courseModelMock.getId()).thenReturn(TEST_ID);
+        when(courseModelMock.getName()).thenReturn(TEST_NAME);
+        when(courseModelMock.getDescription()).thenReturn(TEST_DESCRIPTION);
+        when(courseModelMock.getTopic()).thenReturn(topicModelMock);
+        reset(topicModelMock);
+        reset(topicEntityMock);
+    }
 
     @Test
     public void checkForEntityAnnotations() {
@@ -119,5 +141,52 @@ public class CourseEntityTest {
         );
     }
 
+    @Test
+    void checkEntityGetters() {
+        CourseEntity courseEntity = new CourseEntity(TEST_ID, TEST_NAME, TEST_DESCRIPTION, topicEntityMock);
 
+        assertEquals(TEST_ID, courseEntity.getId());
+        assertEquals(TEST_NAME, courseEntity.getName());
+        assertEquals(TEST_DESCRIPTION, courseEntity.getDescription());
+        assertEquals(topicEntityMock, courseEntity.getTopic());
+    }
+
+    @Test
+    void checkEntitySetters() {
+        CourseEntity courseEntity = new CourseEntity();
+
+        courseEntity.setId(TEST_ID);
+        courseEntity.setName(TEST_NAME);
+        courseEntity.setDescription(TEST_DESCRIPTION);
+        courseEntity.setTopic(topicEntityMock);
+
+        assertEquals(TEST_ID, courseEntity.getId());
+        assertEquals(TEST_NAME, courseEntity.getName());
+        assertEquals(TEST_DESCRIPTION, courseEntity.getDescription());
+        assertEquals(topicEntityMock, courseEntity.getTopic());
+
+        courseEntity.setId(null);
+        assertEquals(TEST_ID, courseEntity.getId());
+    }
+
+    @Test
+    void checkEntityInstantiatedByModel() {
+        final String TOPIC_TEST_ID = "topic test id";
+        final String TOPIC_TEST_NAME = "topic test name";
+        final String TOPIC_TEST_DESCRIPTION = "topic test description";
+
+        when(topicModelMock.getId()).thenReturn(TOPIC_TEST_ID);
+        when(topicModelMock.getName()).thenReturn(TOPIC_TEST_NAME);
+        when(topicModelMock.getDescription()).thenReturn(TOPIC_TEST_DESCRIPTION);
+
+        CourseEntity courseEntity = new CourseEntity(courseModelMock);
+
+        assertEquals(TEST_ID, courseEntity.getId());
+        assertEquals(TEST_NAME, courseEntity.getName());
+        assertEquals(TEST_DESCRIPTION, courseEntity.getDescription());
+
+        assertEquals(TOPIC_TEST_ID, courseEntity.getTopic().getId());
+        assertEquals(TOPIC_TEST_NAME, courseEntity.getTopic().getName());
+        assertEquals(TOPIC_TEST_DESCRIPTION, courseEntity.getTopic().getDescription());
+    }
 }
